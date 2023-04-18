31506fab1f7b42141d75e0b7aea92f4e92809d31
mtvzee <pgzv01@gmail.com>
2023-04-18 19:39:17 +0900

    レスポンシブデザインへの対応と検索欄のUIの変更

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -3,14 +3,13 @@ import styles from '../styles/components/SearchForm.module.css';
 
 export const SearchForm = () => {
   return (
-    <form className={styles.searchForm}>
+    <form className={styles.container}>
       <AiOutlineSearch className={styles.searchIcon} />
       <input
         type="search"
         placeholder="タイトルを検索"
         className={styles.searchInput}
       />
-      <i className={styles.searchBg} />
     </form>
   );
 };
