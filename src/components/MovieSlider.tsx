103b4ca60c1be99de67b9b9c4680568fda51bcd6
mtvzee <pgzv01@gmail.com>
2023-05-19 19:11:47 +0900

    スクロール機能の実装

--- src/components/MovieSlider.tsx
+++ src/components/MovieSlider.tsx
@@ -1,21 +1,66 @@
-import { TbCircleChevronLeft, TbCircleChevronRight } from 'react-icons/tb';
+import throttle from 'lodash/throttle';
+import { useRef, useState } from 'react';
+import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 import styles from '../styles/components/MovieSlider.module.css';
 import { MovieSlide } from './MovieSlide';
 
 export const MovieSlider = () => {
+  const [sliderPosition, setSliderPosition] = useState<
+    'first' | 'middle' | 'last'
+  >('first');
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  const handleClickChevron = (direction: string) => {
+    if (sliderRef.current) {
+      const { scrollLeft, clientWidth } = sliderRef.current;
+      const scrollPosition =
+        direction === 'prev'
+          ? scrollLeft - clientWidth * 0.92
+          : scrollLeft + clientWidth * 0.92;
+      sliderRef.current.scrollTo({ left: scrollPosition, behavior: 'smooth' });
+    }
+  };
+
+  const handleScrollX = throttle(() => {
+    if (sliderRef.current) {
+      const { scrollLeft, clientWidth, scrollWidth } = sliderRef.current;
+      if (scrollLeft === 0) {
+        setSliderPosition('first');
+      } else if (scrollLeft + clientWidth === scrollWidth) {
+        setSliderPosition('last');
+      } else {
+        setSliderPosition('middle');
+      }
+    }
+  }, 200);
+
   return (
     <div className={styles.slider}>
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        ref={sliderRef}
+        onScroll={handleScrollX}
+      >
         {[...Array(12)].map((movie, index) => (
           <MovieSlide key={index} />
         ))}
       </div>
-      <button className={styles.prevBtn}>
-        <TbCircleChevronLeft className={styles.btnIcon} />
-      </button>
-      <button className={styles.nextBtn}>
-        <TbCircleChevronRight className={styles.btnIcon} />
-      </button>
+      {sliderPosition !== 'first' && (
+        <button
+          className={styles.prevBtn}
+          onClick={() => handleClickChevron('prev')}
+        >
+          <BiChevronLeft className={styles.btnIcon} />
+        </button>
+      )}
+      {sliderPosition !== 'last' && (
+        <button
+          className={styles.nextBtn}
+          onClick={() => handleClickChevron('next')}
+        >
+          <BiChevronRight className={styles.btnIcon} />
+        </button>
+      )}
     </div>
   );
 };
